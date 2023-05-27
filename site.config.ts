5d62bcf3d0e8b0b5f3be08422d64dca0eafb3732
YuaYagira <2831352144@qq.com>
2023-05-27 18:07:23 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Blog',
+  name: 'Yuay\' Notion Blog',
   domain: 'blog.yuay.com.cn',
   author: 'Yuay',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Yuay\' Next.js Notion Blog Site',
 
   // social usernames (optional)
-  twitter: '',
-  github: '',
-  linkedin: '',
+  //twitter: '',
+  github: 'YuayYeonhi',
+  // linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
